b04fc87501702de13825bdc700f5c8b4eb9fbd14
Jatt0097 <ekonaam94@gmail.com>
2026-09-15 15:38:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -215,11 +215,6 @@
     function renderLbPhoto() {
       lbImage.src = lbPhotos[lbIndex];
       lbImage.alt = lbTitleText;
-      lbImage.style.objectFit = 'contain';
-      lbImage.style.maxWidth = '100%';
-      lbImage.style.maxHeight = '100%';
-      lbImage.style.width = 'auto';
-      lbImage.style.height = 'auto';
       lbTitle.textContent = lbTitleText;
       var multi = lbPhotos.length > 1;
       lbMeta.textContent = multi
